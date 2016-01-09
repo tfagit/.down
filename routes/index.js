a80eef3ed486e195ba67a98e6ec86c45609882e2
Victor Diniz <blek.net@gmail.com>
2016-01-09 18:31:14 -0200

    wget now works properly

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
             err.status = 404;
             return next(err);
         }
-        var url_with_host = path.join("http://" + req.headers.host, req.url);
+        var url_with_host = "http://" + req.headers.host + req.url;
         dir.addCommandsToContents(contents, url_with_host);
         res.render('index', {
             title: '.down',
